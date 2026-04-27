5ce7c6d736168bc124385854b55fff80a642c81f
roblox2 <a18876919000@outlook.com>
2026-04-27 23:27:46 +0800

    修改大部分功能添加部分新壁纸

--- images.js
+++ images.js
@@ -1,81 +1,40 @@
-
 const IMAGE_LIST = [
 
-
-
     // ========== 原神类 ==========
-    
-    // 萤的图片
     { url: "https://dl3.img.timecdn.cn/2025/11/06/1000008653.webp", tags: ["萤", "休闲装", "原神"] },
     { url: "https://dl2.img.timecdn.cn/2025/11/06/1000008415.webp", tags: ["萤", "内衣", "猫耳", "猫娘", "泳衣", "原神"] },
     { url: "https://dl2.img.timecdn.cn/2025/11/06/1000008461.webp", tags: ["内衣", "泳装", "萤", "原神"] },
     { url: "https://dl3.img.timecdn.cn/2025/11/06/1000008572.webp", tags: ["内衣", "泳装", "萤", "原神"] },
     { url: "https://dl3.img.timecdn.cn/2025/11/06/1000008584.webp", tags: ["萤", "泳装", "原神"] },
-    
-    // 八重神子的图片
     { url: "https://dl2.img.timecdn.cn/2025/11/06/1000008662.jpg", tags: ["八重神子", "神子", "大奶子", "原神"] },
-    
-    // 芙宁娜的图片
     { url: "https://dl.img.timecdn.cn/2025/11/06/1000008648.webp", tags: ["芙宁娜", "连体衣", "原神"] },
-    
-    // 妮露的图片
     { url: "https://dl3.img.timecdn.cn/2025/11/06/1000008617.webp", tags: ["妮露", "泳装", "原神"] },
-    
-    // 神里绫华的图片
     { url: "https://dl3.img.timecdn.cn/2025/11/06/1000008651.webp", tags: ["神里绫华", "泳衣", "原神"] },
-    
-    // 茜特菈莉的图片
     { url: "https://dl3.img.timecdn.cn/2025/11/06/1000008395.webp", tags: ["茜特菈莉", "兔女郎", "原神", "连体衣"] },
-    
-    // 心海的图片
     { url: "https://dl3.img.timecdn.cn/2025/11/06/1000008656.webp", tags: ["心海", "内衣", "原神"] },
     
-    
     // ========== 蔚蓝档案类 ==========
-    
-    // 小春的图片
     { url: "https://dl2.img.timecdn.cn/2025/11/06/bg2.jpg", tags: ["小春", "色色", "蔚蓝档案"] },
-    
-    // 玛丽的图片
     { url: "https://dl3.img.timecdn.cn/2025/11/06/bg4.jpg", tags: ["玛丽", "泳衣", "连体衣", "蔚蓝档案"] },
     { url: "https://dl.img.timecdn.cn/2025/11/06/bg8.jpg", tags: ["玛丽", "猫娘", "内衣", "蔚蓝档案"] },
     { url: "https://dl3.img.timecdn.cn/2025/11/06/1000012007.jpg", tags: ["伊落玛丽", "玛丽", "猫娘", "蔚蓝档案"] },
-    
-    // 清澄晶的图片
     { url: "https://dl.img.timecdn.cn/2025/11/06/bg6.jpg", tags: ["清澄晶", "猫娘", "比基尼", "蔚蓝档案"] },
     { url: "https://dl.img.timecdn.cn/2025/11/06/bg9.jpg", tags: ["清澄晶", "大奶子", "创可贴", "蔚蓝档案"] },
-    
-    // 早濑优香的图片
     { url: "https://dl.img.timecdn.cn/2025/11/06/bg7.jpg", tags: ["早濑优香", "猫娘", "比基尼", "蔚蓝档案"] },
-    
-    // 各务千寻的图片
     { url: "https://dl2.img.timecdn.cn/2025/11/06/bg10.jpg!h.webp", tags: ["各务千寻", "比基尼", "连体衣", "蔚蓝档案"] },
-    
-    // 杏山和纱的图片
     { url: "https://dl3.img.timecdn.cn/2025/11/06/bg11.jpg!h.webp", tags: ["杏山和纱", "猫娘", "比基尼", "蔚蓝档案"] },
     { url: "https://dl2.img.timecdn.cn/2025/11/06/bg26.png", tags: ["杏山和纱", "杏山カズサ", "比基尼", "泳衣", "蔚蓝档案"] },
     { url: "杏山和纱18+", tags: ["杏山和纱","杏山","和纱","蔚蓝档案"] },
-    
-    // 砂狼白子的图片
     { url: "https://dl.img.timecdn.cn/2025/11/06/bg13.png", tags: ["砂狼白子", "砂狼シロコ", "同人", "蔚蓝档案"] },
     { url: "https://dl.img.timecdn.cn/2025/11/06/1000008633.webp", tags: ["砂狼白子", "白子", "蔚蓝档案"] },
-    
-    // 水羽三森的图片
     { url: "https://dl.img.timecdn.cn/2025/11/06/bg14.png", tags: ["三森", "水羽三森", "水羽ミモリ", "泳衣", "星空", "幻想", "蔚蓝档案"] },
-    
-    // 狐坂若藻的图片
     { url: "https://dl2.img.timecdn.cn/2025/11/06/bg15.jpg", tags: ["狐坂若藻", "水若藻", "泳装", "夏日", "狐坂ワカモ", "蔚蓝档案"] },
-    
-    // 小鸟游星野的图片
     { url: "https://dl2.img.timecdn.cn/2025/11/06/bg25.png", tags: ["小鸟游星野", "星野", "可爱", "和服", "蔚蓝档案"] },
     { url: "https://dl3.img.timecdn.cn/2025/11/06/1000012783.jpg", tags: ["小鸟游星野", "星野", "比基尼", "蔚蓝档案"] },
     { url: "小鸟游18+1", tags: ["小鸟游星野","星野","百合","蔚蓝档案"] },
     { url: "小鸟游18+2", tags: ["小鸟游星野","星野","百合","蔚蓝档案"] },
     { url: "小鸟游18+3", tags: ["小鸟游星野","星野","百合","蔚蓝档案"] },
-    // 调月莉音的图片
     { url: "https://dl2.img.timecdn.cn/2025/11/06/bg27.jpg", tags: ["调月莉音", "调月リオ", "色色", "蔚蓝档案"] },
-    
-    // 圆堂志美子的图片
     { url: "https://dl3.img.timecdn.cn/2025/11/06/illust_137166340_20251106_203857.jpg", tags: ["圆堂志美子", "连体衣", "蔚蓝档案"] },
     { url: "https://dl3.img.timecdn.cn/2025/11/06/illust_137166340_20251106_203850.jpg", tags: ["圆堂志美子", "连体衣", "蔚蓝档案"] },
     { url: "https://dl.img.timecdn.cn/2025/11/06/illust_137166340_20251106_203846.jpg", tags: ["圆堂志美子", "连体衣", "蔚蓝档案"] },
@@ -85,45 +44,24 @@ const IMAGE_LIST = [
     { url: "https://dl2.img.timecdn.cn/2025/11/06/illust_137166340_20251106_203831.jpg", tags: ["圆堂志美子", "连体衣", "蔚蓝档案"] },
     { url: "https://dl2.img.timecdn.cn/2025/11/06/illust_137166340_20251106_203609.jpg", tags: ["圆堂志美子", "连体衣", "蔚蓝档案"] },
     { url: "https://dl3.img.timecdn.cn/2025/11/06/illust_137166340_20251106_203607.jpg", tags: ["圆堂志美子", "连体衣", "蔚蓝档案"] },
-    
-    // 一之赖明日奈的图片
     { url: "https://dl3.img.timecdn.cn/2025/11/06/illust_137166538_20251106_203558.png", tags: ["一之赖明日奈", "比基尼", "蔚蓝档案"] },
-    
-    // 空崎日奈的图片
     { url: "https://dl3.img.timecdn.cn/2025/11/06/1000012771.jpg", tags: ["空崎日奈", "日奈", "黑丝", "比基尼", "蔚蓝档案"] },
-    
-    // 天雨亚子的图片
     { url: "https://dl.img.timecdn.cn/2025/11/06/1000008624.webp", tags: ["天雨亚子", "天雨アコ", "黄昏", "蔚蓝档案"] },
-    
-    // 浦和花子的图片
     { url: "https://dl.img.timecdn.cn/2025/11/06/1000012003.jpg", tags: ["浦和花子", "花子", "蔚蓝档案"] },
     
-    //=========== 绝区零 ==========
-    //艾莲乔的图片
+    // ========== 绝区零 ==========
     { url: "艾莲乔", tags: ["艾莲乔","艾莲","绝区零"] },
     
     // ========== 崩坏类 ==========
-    
-    // 流萤的图片
     { url: "https://dl2.img.timecdn.cn/2025/11/06/1000008583.jpg", tags: ["流萤", "内衣", "崩坏"] },
     { url: "https://dl3.img.timecdn.cn/2025/11/06/1000008472.webp", tags: ["流萤", "萤", "崩坏"] },
-    
-    //爱莉希雅的图片
     { url: "爱莉希雅18+", tags: ["爱莉希雅","R18","崩坏"] },
-    
-    // 风堇的图片
     { url: "https://dl2.img.timecdn.cn/2025/11/06/1000008360.webp", tags: ["风堇", "雅辛忒丝", "崩坏"] },
     
-    
     // ========== 初音未来类 ==========
-    
-    // Miku的图片
     { url: "https://dl.img.timecdn.cn/2025/11/06/1000024131.jpg", tags: ["初音未来", "Miku", "比基尼"] },
     
-    
     // ========== 通用/角色类 ==========
-    
-    //猫娘扶她的图片
     { url: "猫娘扶她", tags: ["扶她","扶她猫娘"] },
     { url: "猫娘扶她1", tags: ["扶她","扶她猫娘"] },
     { url: "猫娘扶她2", tags: ["扶她","扶她猫娘"] },
@@ -134,30 +72,17 @@ const IMAGE_LIST = [
     { url: "猫娘扶她7", tags: ["扶她","扶她猫娘"] },
     { url: "猫娘扶她8", tags: ["扶她","扶她猫娘"] },
     { url: "胶衣扶她", tags: ["扶她","扶她猫娘"] },
-    
-    //小马扶她的图片
     { url: "小马扶她", tags: ["扶她","扶她小马"] },
-    
-    // 好看的猫娘的图片
     { url: "猫娘18+", tags: ["色色猫娘","R18"] },
     { url: "猫娘18+1", tags: ["色色猫娘","R18"] },
     { url: "猫娘18+2", tags: ["色色猫娘","R18"] },
-    
-    // Fuwawa Abyssgard的图片
     { url: "https://dl3.img.timecdn.cn/2025/11/06/bg5.jpg", tags: ["Fuwawa Abyssgard", "猫娘", "内衣"] },
-    
-    
-    
-    //小男娘的图片和视频
     { url: "小男娘", tags: ["扶她","男娘","R18"] },
     { url: "小男娘1", tags: ["扶她","男娘","R18"] },
     { url: "小男娘2", tags: ["扶她","男娘","R18"] },
     { url: "小男娘3", tags: ["扶她","男娘","R18"] },
     
-    
-    // ========== 其他/杂项类 ==========
-    
-    // 魅魔类的图片
+    // ========== 魅魔类 ==========
     { url: "https://dl2.img.timecdn.cn/2025/11/06/bg16.jpg", tags: ["魅魔", "内衣", "色色"] },
     { url: "https://dl.img.timecdn.cn/2025/11/06/illust_110008348_20251106_020847.jpg", tags: ["魅魔", "内衣", "色色"] },
     { url: "https://dl3.img.timecdn.cn/2025/11/06/bg18.jpg", tags: ["魅魔", "内衣", "色色"] },
@@ -195,13 +120,11 @@ const IMAGE_LIST = [
     // 害羞少女
     { url: "https://dl2.img.timecdn.cn/2025/11/06/illust_137158820_20251106_204109.jpg", tags: ["害羞", "少女", "蔚蓝档案"] },
     
-    
     // ========== 腹肌类 ==========
     { url: "腹肌1", tags: ["腹肌"] },
     { url: "腹肌2", tags: ["腹肌"] },
     { url: "腹肌3", tags: ["腹肌"] },
     { url: "腹肌4", tags: ["腹肌"] },
-    
     
     // ========== 福瑞类 ==========
     { url: "https://dl2.img.timecdn.cn/2025/11/06/1000012580.jpg", tags: ["福瑞", "连体衣", "可爱"] },
@@ -219,28 +142,237 @@ const IMAGE_LIST = [
     { url: "https://dl.img.timecdn.cn/2025/11/06/1000012871.jpg", tags: ["二次元", "宅女", "比基尼"] },
     { url: "https://dl3.img.timecdn.cn/2025/11/06/1000012776.jpg", tags: ["二次元", "比基尼", "可爱"] },
     { url: "https://dl2.img.timecdn.cn/2025/11/06/1000012835.jpg", tags: ["色色", "连体衣", "横屏"] },
-];
+    
+    {
+    url: "bg1",          
+    layer2: "bg1_detail", 
+    tags: ["R18", "双层"]
+    },
+ 
+   {
+    url: "bg2",          
+    layer2: "bg2_detail", 
+    tags: ["测试双层","R18", "双层"]
+    },
 
+    {
+    url: "bg3",           
+    layer2: "bg3_detail", 
+    tags: ["测试双层","R18", "双层"]
+    },
+    {
+    url: "bg4",           
+    layer2: "bg4_detail", 
+    tags: ["测试双层","R18", "双层"]
+    },
+    {
+    url: "bg5",           
+    layer2: "bg5_detail", 
+    tags: ["测试双层","R18", "双层"]
+    },
+    {
+    url: "bg6",           
+    layer2: "bg6_detail", 
+    tags: ["测试双层","R18", "双层"]
+    },
+    {
+    url: "bg7",           
+    layer2: "bg7_detail", 
+    tags: ["测试双层","R18", "双层"]
+    },
+    {
+    url: "bg8",           
+    layer2: "bg8_detail", 
+    tags: ["测试双层","R18", "双层"]
+    },
+    {
+    url: "bg9",           
+    layer2: "bg9_detail", 
+    tags: ["测试双层","R18", "双层"]
+    },
+    {
+    url: "bg10",           
+    layer2: "bg10_detail", 
+    tags: ["测试双层","R18", "双层"]
+    },
+    {
+    url: "bg11",           
+    layer2: "bg11_detail", 
+    tags: ["测试双层","R18", "双层"]
+    },
+    {
+    url: "bg12",           
+    layer2: "bg12_detail", 
+    tags: ["测试双层","R18", "双层"]
+    },
+    {
+    url: "bg13",           
+    layer2: "bg13_detail", 
+    tags: ["测试双层","R18", "双层"]
+    },
+    {
+    url: "bg14",           
+    layer2: "bg14_detail", 
+    tags: ["测试双层","R18", "双层"]
+    },
+    {
+    url: "bg15",           
+    layer2: "bg15_detail", 
+    tags: ["测试双层","R18", "双层"]
+    },
+    {
+    url: "bg16",           
+    layer2: "bg16_detail", 
+    tags: ["测试双层","R18", "双层"]
+    },
+    {
+    url: "bg17",           
+    layer2: "bg17_detail", 
+    tags: ["测试双层","R18", "双层"]
+    },
+    {
+    url: "bg18",           
+    layer2: "bg18_detail", 
+    tags: ["测试双层","R18", "双层"]
+    },
+    {
+    url: "bg19",           
+    layer2: "bg19_detail", 
+    tags: ["测试双层","R18", "双层"]
+    },
+    {
+    url: "bg20",           
+    layer2: "bg20_detail", 
+    tags: ["测试双层","R18", "双层"]
+    },
+    {
+    url: "bg21",           
+    layer2: "bg21_detail", 
+    tags: ["测试双层","R18", "双层"]
+    },
+    {
+    url: "bg22",           
+    layer2: "bg22_detail", 
+    tags: ["测试双层","R18", "双层"]
+    },
+    {
+    url: "bg23",           
+    layer2: "bg23_detail", 
+    tags: ["测试双层","R18", "双层"]
+    },
+    {
+    url: "bg24",           
+    layer2: "bg24_detail", 
+    tags: ["测试双层","R18", "双层"]
+    },
+    {
+    url: "bg25",           
+    layer2: "bg25_detail", 
+    tags: ["测试双层","R18", "双层"]
+    },
+    {
+    url: "bg26",           
+    layer2: "bg26_detail", 
+    tags: ["测试双层","R18", "双层"]
+    },
+    {
+    url: "bg27",           
+    layer2: "bg27_detail", 
+    tags: ["测试双层","R18", "双层"]
+    },
+    {
+    url: "bg28",           
+    layer2: "bg28_detail", 
+    tags: ["测试双层","R18", "双层"]
+    },
+    {
+    url: "bg29",           
+    layer2: "bg29_detail", 
+    tags: ["测试双层","R18", "双层"]
+    },
+    {
+    url: "bg30",           
+    layer2: "bg30_detail", 
+    tags: ["测试双层","R18", "双层"]
+    },
+    {
+    url: "bg31",           
+    layer2: "bg31_detail", 
+    tags: ["测试双层","R18", "双层"]
+    },
+    {
+    url: "bg32",           
+    layer2: "bg32_detail", 
+    tags: ["测试双层","R18", "双层"]
+    },
+    {
+    url: "bg33",           
+    layer2: "bg33_detail", 
+    tags: ["测试双层","R18", "双层"]
+    },
+    {
+    url: "bg34",           
+    layer2: "bg34_detail", 
+    tags: ["测试双层","R18", "双层"]
+    },
+    {
+    url: "bg35",           
+    layer2: "bg35_detail", 
+    tags: ["测试双层","R18", "双层"]
+    },
+    {
+    url: "bg36",           
+    layer2: "bg36_detail", 
+    tags: ["测试双层","R18", "双层"]
+    },
+    {
+    url: "bg37",           
+    layer2: "bg37_detail", 
+    tags: ["测试双层","R18", "双层"]
+    },
+    {
+    url: "bg38",           
+    layer2: "bg38_detail", 
+    tags: ["测试双层","R18", "双层"]
+    },
+    {
+    url: "bg39",           
+    layer2: "bg39_detail", 
+    tags: ["测试双层","R18", "双层"]
+    },
+    {
+    url: "bg40",           
+    layer2: "bg40_detail", 
+    tags: ["测试双层","R18", "双层"]
+    },
+    {
+    url: "bg41",           
+    layer2: "bg41_detail", 
+    tags: ["测试双层","R18", "双层"]
+    },
+  {
+    url: "bg33.jpg",           
+    layer2: "bg33_detail.jpg", 
+    tags: ["1测试","R18", "双层"]
+    },
+];
 
 // ========= 视频 =========
 IMAGE_LIST.push(
-
-    //虚拟主播凑阿库娅的图片或视频
-    { url: "凑阿库娅", tags: ["凑阿库娅","操逼","R18","视频"] ,isVideo: true },
-    { url: "凑阿库娅1", tags: ["凑阿库娅","操逼","R18","视频"] ,isVideo: true },
-    
-    //男娘的视频
-    { url: "小男娘4", tags: ["扶她","男娘","R18","视频"] ,isVideo: true},
-    { url: "小男娘5", tags: ["扶她","男娘","R18","视频"] ,isVideo: true},
-    { url: "大鸡男娘", tags: ["大鸡鸡","扶她","男娘","R18","视频"] ,isVideo: true},
-    
+    { url: "凑阿库娅", tags: ["凑阿库娅","操逼","R18","视频"], isVideo: true },
+    { url: "凑阿库娅1", tags: ["凑阿库娅","操逼","R18","视频"], isVideo: true },
+    { url: "小男娘4", tags: ["扶她","男娘","R18","视频"], isVideo: true },
+    { url: "小男娘5", tags: ["扶她","男娘","R18","视频"], isVideo: true },
+    { url: "大鸡男娘", tags: ["大鸡鸡","扶她","男娘","R18","视频"], isVideo: true },
+    { url: "迪莫18+", tags: ["迪莫","大鸡鸡","洛克王国"], isVideo: true },
+    { url: "测试", tags: ["测试"], isVideo: true }
 );
 
 console.log(`✅ 已添加视频，现在共有 ${IMAGE_LIST.length} 个媒体`);
 
-// 纯URL数组（供其他地方使用）
 const IMAGE_LIST_RAW = IMAGE_LIST.map(item => item.url);
 
-// 控制台输出统计信息
 console.log(`✅ 图片库加载完成：共 ${IMAGE_LIST.length} 张图片`);
 console.log(`📋 分类：原神类、蔚蓝档案类、崩坏类、初音未来类、腹肌类、福瑞类等`);
+// 修复：删除引用未定义变量 doubleLayerImages 的语句
+// 原错误行已移除
